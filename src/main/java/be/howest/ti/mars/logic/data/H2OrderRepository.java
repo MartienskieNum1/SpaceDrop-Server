89e4c45cb4db81a9de47b9928d2b254eda8c3a3b
Thibaut Lenoir <thibaut.lenoir@student.howest.be>
2020-11-11 10:40:30 +0100

    Add order repo for H2 databases

---
+++ src/main/java/be/howest/ti/mars/logic/data/H2OrderRepository.java
@@ -0,0 +1,12 @@
+package be.howest.ti.mars.logic.data;
+
+import be.howest.ti.mars.logic.domain.Order;
+
+import java.util.List;
+
+public class H2OrderRepository implements OrderRepository {
+    @Override
+    public List<Order> getOrders() {
+        return null;
+    }
+}
