4eb572a851f9f2da787f1e45ab7838c47b4fb2a9
Martienskie <maarten.de.meyere@student.howest.be>
2020-11-11 13:39:16 +0100

    return newly made user on createUser

--- src/main/java/be/howest/ti/mars/logic/controller/MarsController.java
+++ src/main/java/be/howest/ti/mars/logic/controller/MarsController.java
@@ -1,7 +1,17 @@
 package be.howest.ti.mars.logic.controller;
 
+import be.howest.ti.mars.logic.data.MarsRepository;
+import be.howest.ti.mars.logic.domain.User;
+import io.vertx.core.json.JsonObject;
+
 public class MarsController {
+
     public String getMessage() {
         return "Hello, Mars!";
+    }
+
+    public JsonObject createUser(User user) {
+        MarsRepository.createUser(user);
+        return JsonObject.mapFrom(user);
     }
 }
