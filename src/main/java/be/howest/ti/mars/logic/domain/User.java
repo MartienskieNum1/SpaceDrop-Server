cabb2ada75a37321ca5a17de48dd4f4fbabb497b
Martienskie <maarten.de.meyere@student.howest.be>
2020-11-11 11:27:39 +0100

    add fields and constructor

--- src/main/java/be/howest/ti/mars/logic/domain/User.java
+++ src/main/java/be/howest/ti/mars/logic/domain/User.java
@@ -1,4 +1,17 @@
 package be.howest.ti.mars.logic.domain;
 
 public class User {
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String email;
+    private String password;
+
+    public User(String firstName, String lastName, String phoneNumber, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.password = password;
+    }
 }
