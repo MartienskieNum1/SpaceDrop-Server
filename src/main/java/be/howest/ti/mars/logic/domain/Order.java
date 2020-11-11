111943193cc39d5dc8cb6028fa6957796d911a1e
Thibaut Lenoir <thibaut.lenoir@student.howest.be>
2020-11-11 09:52:46 +0100

    Add fields and constructor

--- src/main/java/be/howest/ti/mars/logic/domain/Order.java
+++ src/main/java/be/howest/ti/mars/logic/domain/Order.java
@@ -1,4 +1,23 @@
 package be.howest.ti.mars.logic.domain;
 
 public class Order {
+    private int orderId;
+    private int userId;
+    private int rocketId;
+    private int statusId;
+
+    private double mass;
+    private double width;
+    private double height;
+    private double cost;
+
+    public Order(int userId, int rocketId, int statusId, double mass, double width, double height, double cost) {
+        this.userId = userId;
+        this.rocketId = rocketId;
+        this.statusId = statusId;
+        this.mass = mass;
+        this.width = width;
+        this.height = height;
+        this.cost = cost;
+    }
 }
