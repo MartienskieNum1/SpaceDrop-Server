c4db1d469c75e69750c03a5f8f353d8f1865ee81
Thibaut Lenoir <thibaut.lenoir@student.howest.be>
2020-11-11 10:59:04 +0100

    Add method to get all orders from the database

--- src/main/java/be/howest/ti/mars/logic/data/H2OrderRepository.java
+++ src/main/java/be/howest/ti/mars/logic/data/H2OrderRepository.java
@@ -2,11 +2,41 @@ package be.howest.ti.mars.logic.data;
 
 import be.howest.ti.mars.logic.domain.Order;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class H2OrderRepository implements OrderRepository {
+
+    private static final String SQL_SELECT_ALL_ORDERS = "select * from orders";
+
     @Override
     public List<Order> getOrders() {
-        return null;
+        List<Order> orders = new ArrayList<>();
+
+        try (Connection con = MarsRepository.getConnection();
+            PreparedStatement stmt = con.prepareStatement(SQL_SELECT_ALL_ORDERS);
+             ResultSet results = stmt.executeQuery()) {
+            while (results.next()) {
+                int orderId = results.getInt("id");
+                int userId = results.getInt("user_id");
+                int rocketId = results.getInt("rocket_id");
+                int statusId = results.getInt("status_id");
+                double mass = results.getDouble("mass");
+                double width = results.getDouble("width");
+                double height = results.getDouble("height");
+                double depth = results.getDouble("depth");
+                double cost = results.getDouble("cost");
+
+                orders.add(new Order(userId, rocketId, statusId, mass, width, height, depth, cost));
+            }
+        } catch (SQLException e) {
+            throw new IllegalStateException();
+        }
+
+        return orders;
     }
 }
--- src/main/java/be/howest/ti/mars/logic/domain/Order.java
+++ src/main/java/be/howest/ti/mars/logic/domain/Order.java
@@ -9,15 +9,17 @@ public class Order {
     private double mass;
     private double width;
     private double height;
+    private double depth;
     private double cost;
 
-    public Order(int userId, int rocketId, int statusId, double mass, double width, double height, double cost) {
+    public Order(int userId, int rocketId, int statusId, double mass, double width, double height, double depth, double cost) {
         this.userId = userId;
         this.rocketId = rocketId;
         this.statusId = statusId;
         this.mass = mass;
         this.width = width;
         this.height = height;
+        this.depth = depth;
         this.cost = cost;
     }
 }
