d0056c2bef679644ed517e5f6c73e35ec45caa6e
Martienskie <maarten.de.meyere@student.howest.be>
2020-11-12 12:57:57 +0100

    make method to verify whether user exists with token

--- src/main/java/be/howest/ti/mars/logic/controller/MarsController.java
+++ src/main/java/be/howest/ti/mars/logic/controller/MarsController.java
@@ -2,26 +2,36 @@ package be.howest.ti.mars.logic.controller;
 
 import be.howest.ti.mars.logic.data.MarsRepository;
 import be.howest.ti.mars.logic.domain.User;
+import be.howest.ti.mars.logic.util.MarsException;
 import be.howest.ti.mars.logic.util.TokenAES;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 
 public class MarsController {
+    private final MarsRepository marsRepository = new MarsRepository();
 
     public String getMessage() {
         return "Hello, Mars!";
     }
 
     public String createUser(User user) {
-        MarsRepository.createUser(user);
+        marsRepository.createUser(user);
         return TokenAES.encrypt(user.getEmail());
+    }
+
+    public boolean userExists(String token) {
+        String email = TokenAES.decrypt(token);
+        try {
+            marsRepository.getUserOnEmail(email);
+            return true;
+        } catch (MarsException ex) {
+            return false;
+        }
     }
 
     public JsonArray getUsers() {
         JsonArray array = new JsonArray();
-        for (User user : MarsRepository.getUsers()) {
-            array.add(JsonObject.mapFrom(user));
-        }
+        marsRepository.getUsers().forEach(user -> array.add(JsonObject.mapFrom(user)));
         return array;
     }
 }
