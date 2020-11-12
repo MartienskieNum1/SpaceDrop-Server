faf0a4b054648335f7898b4368c873f5e0dbec81
Martienskie <maarten.de.meyere@student.howest.be>
2020-11-12 13:40:51 +0100

    make connection protected static

--- src/main/java/be/howest/ti/mars/logic/data/MarsRepository.java
+++ src/main/java/be/howest/ti/mars/logic/data/MarsRepository.java
@@ -118,7 +118,7 @@ public class MarsRepository {
         }
     }
 
-    private Connection getConnection() throws SQLException {
+    protected static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(INSTANCE.url, INSTANCE.username, INSTANCE.password);
     }
 }
