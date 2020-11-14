dd56a67dcb2551ee5015914bb7dc8911c0f511f4
Martienskie <maarten.de.meyere@student.howest.be>
2020-11-14 17:37:27 +0100

    implement login call in WebServer

--- src/main/java/be/howest/ti/mars/webserver/WebServer.java
+++ src/main/java/be/howest/ti/mars/webserver/WebServer.java
@@ -140,6 +140,7 @@ public class WebServer extends AbstractVerticle {
         addRouteWithCtxFunction(factory, "getMessage", bridge::getMessage);
         addRouteWithCtxFunction(factory, "createUser", bridge::createUser);
         addRouteWithCtxFunction(factory, "getUsers", bridge::getUsers);
+        addRouteWithCtxFunction(factory, "login", bridge::login);
         addRouteWithCtxFunction(factory, "getOrders", bridge::getOrders);
         addRouteWithCtxFunction(factory, "createOrder", bridge::createOrder);
     }
