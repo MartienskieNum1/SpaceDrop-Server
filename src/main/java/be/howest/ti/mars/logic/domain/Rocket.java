78a70605142390c16e39a85c01a3290aaab906a8
Martienskie <maarten.de.meyere@student.howest.be>
2020-11-19 10:49:29 +0100

    fix how date is allocated

--- src/main/java/be/howest/ti/mars/logic/domain/Rocket.java
+++ src/main/java/be/howest/ti/mars/logic/domain/Rocket.java
@@ -1,23 +1,27 @@
 package be.howest.ti.mars.logic.domain;
 
-import java.time.format.DateTimeFormatter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Objects;
 
 public class Rocket {
+    private SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+
     private final String name;
-    private final DateTimeFormatter departure;
-    private final DateTimeFormatter arrival;
+    private final Date departure;
+    private final Date arrival;
     private final float pricePerKilo;
     private final float maxMass;
     private final float maxVolume;
     private float availableMass;
     private float availableVolume;
 
-    public Rocket(String name, DateTimeFormatter departure, DateTimeFormatter arrival, float pricePerKilo, float maxMass,
-                  float maxVolume) {
+    public Rocket(String name, String departure, String arrival, float pricePerKilo, float maxMass,
+                  float maxVolume) throws ParseException {
         this.name = name;
-        this.departure = departure;
-        this.arrival = arrival;
+        this.departure = format.parse(departure);
+        this.arrival = format.parse(arrival);
         this.pricePerKilo = pricePerKilo;
         this.maxMass = maxMass;
         this.maxVolume = maxVolume;
@@ -29,11 +33,11 @@ public class Rocket {
         return name;
     }
 
-    public DateTimeFormatter getDeparture() {
+    public Date getDeparture() {
         return departure;
     }
 
-    public DateTimeFormatter getArrival() {
+    public Date getArrival() {
         return arrival;
     }
 
