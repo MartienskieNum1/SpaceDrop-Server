629a28dbe288dd83285294bbaebff1905ec69012
Martienskie <maarten.de.meyere@student.howest.be>
2020-12-04 14:27:58 +0100

    use instance in Repositories

--- src/main/java/be/howest/ti/mars/logic/data/Repositories.java
+++ src/main/java/be/howest/ti/mars/logic/data/Repositories.java
@@ -2,7 +2,7 @@ package be.howest.ti.mars.logic.data;
 
 public class Repositories {
 
-    public static final MarsRepository H2REPO = new H2Repository();
+    public static final MarsRepository H2REPO = H2Repository.getInstance();
 
     private Repositories() {
     }
