5fc5e2f8d3ad13338a827de46df8580ad96afde0
Martienskie <maarten.de.meyere@student.howest.be>
2020-12-06 19:51:48 +0100

    create generalUser + implement login tests

--- src/test/java/be/howest/ti/mars/logic/controller/MarsControllerTest.java
+++ src/test/java/be/howest/ti/mars/logic/controller/MarsControllerTest.java
@@ -5,7 +5,9 @@ import be.howest.ti.mars.logic.domain.Address;
 import be.howest.ti.mars.logic.domain.User;
 import be.howest.ti.mars.logic.util.TokenAES;
 import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mindrot.jbcrypt.BCrypt;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -24,28 +26,46 @@ class MarsControllerTest {
     }
 
     MarsController controller = new MarsController(Repositories.MOCK_REPO);
+    User generalUser;
+
+    @BeforeEach
+    void populate() {
+        Address address = new Address("Earth", "Belgium", "City", "Street", 0);
+        User user1 = new User("Maarten", "De Meyere", "0412345678", "maarten@maarten", "pass", address);
+        generalUser = user1;
+
+        controller.createUser(user1);
+    }
 
     @Test
     void createUserReturnsToken() {
         Address address = new Address("Earth", "Belgium", "City", "Street", 0);
-        User user = new User("Maarten", "De Meyere", "0412345678", "maarten@maarten", "pass", address);
+        User user = new User("Maarten", "De Meyere", "0412345678", "maarten1@maarten1", "pass", address);
 
         assertEquals(TokenAES.encrypt(user.getEmail()), controller.createUser(user));
     }
 
     @Test
-    void createUserReturnsNull() {
+    void createUserReturnsNull() { //email is same of generalUser
         Address address = new Address("Earth", "Belgium", "City", "Street", 0);
-        User user1 = new User("Maarten", "De Meyere", "0412345678", "maarten@maarten", "pass", address);
-        User user2 = new User("Mira", "Vogelsang", "0412345678", "maarten@maarten", "pass", address);
+        User user = new User("Mira", "Vogelsang", "0412345678", "maarten@maarten", "pass", address);
 
-        controller.createUser(user1);
-
-        assertNull(controller.createUser(user2));
+        assertNull(controller.createUser(user));
     }
 
     @Test
     void successfulLogin() {
+        assertEquals(TokenAES.encrypt(generalUser.getEmail()), controller.login(generalUser.getEmail(), generalUser.getPassword()));
+    }
+
+    @Test
+    void wrongPassLogin() {
+        assertNull(controller.login(generalUser.getEmail(), BCrypt.hashpw("randomPass", BCrypt.gensalt())));
+    }
+
+    @Test
+    void wrongEmailLogin() {
+        assertNull(controller.login("Random@Email", generalUser.getPassword()));
     }
 
     @Test
