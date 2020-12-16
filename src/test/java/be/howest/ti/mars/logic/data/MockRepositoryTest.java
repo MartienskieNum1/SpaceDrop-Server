61c5dbc1225993f0c09b52ae0b68620bdfe72d0e
Martienskie <maarten.de.meyere@student.howest.be>
2020-12-16 13:14:39 +0100

    mod all tests with new order

--- src/test/java/be/howest/ti/mars/logic/data/MockRepositoryTest.java
+++ src/test/java/be/howest/ti/mars/logic/data/MockRepositoryTest.java
@@ -79,15 +79,15 @@ class MockRepositoryTest {
     @Test
     void createOrder() {
         Address address = new Address("Earth", "Belgium", "City", "Street", 0);
-        repo.createOrder(new Order(1, UUID.randomUUID(), 1, 1, 1, 500, 500, 500, 500, 500, address), 1);
+        repo.createOrder(new Order(1, UUID.randomUUID(), 1, 1, 1, "status", 500, 500, 500, 500, 500, address), 1);
         assertEquals(1, repo.getOrders().size());
     }
 
     @Test
     void getOrderById() {
         Address address = new Address("Earth", "Belgium", "City", "Street", 0);
-        Order order1 = new Order(1, UUID.randomUUID(), 1, 1, 1, 500, 500, 500, 500, 500, address);
-        Order order2 = new Order(5, UUID.randomUUID(), 1, 1, 1, 500, 500, 500, 500, 500, address);
+        Order order1 = new Order(1, UUID.randomUUID(), 1, 1, 1, "status", 500, 500, 500, 500, 500, address);
+        Order order2 = new Order(5, UUID.randomUUID(), 1, 1, 1, "status", 500, 500, 500, 500, 500, address);
         repo.createOrder(order1, 1);
         repo.createOrder(order2, 1);
 
@@ -98,9 +98,9 @@ class MockRepositoryTest {
     void getOrdersForUser() {
         Address address = new Address("Earth", "Belgium", "City", "Street", 0);
         User user1 = new User(1, "Maarten", "De Meyere", "0412345678", "maarten@maarten", "pass", address);
-        Order order1 = new Order(1, UUID.randomUUID(), 1, 1, 1, 500, 500, 500, 500, 500, address);
-        Order order2 = new Order(5, UUID.randomUUID(), 1, 1, 1, 500, 500, 500, 500, 500, address);
-        Order order3 = new Order(2, UUID.randomUUID(), 2, 1, 1, 500, 500, 500, 500, 500, address);
+        Order order1 = new Order(1, UUID.randomUUID(), 1, 1, 1, "status", 500, 500, 500, 500, 500, address);
+        Order order2 = new Order(5, UUID.randomUUID(), 1, 1, 1, "status", 500, 500, 500, 500, 500, address);
+        Order order3 = new Order(2, UUID.randomUUID(), 2, 1, 1, "status", 500, 500, 500, 500, 500, address);
 
         repo.createUser(user1);
         repo.createOrder(order1, 1);
