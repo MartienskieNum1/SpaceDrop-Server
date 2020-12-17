d8a207fff906afb8268123ee943f053e0a74a2f4
Martienskie <maarten.de.meyere@student.howest.be>
2020-12-17 17:10:47 +0100

    orders will be updated

--- src/main/java/be/howest/ti/mars/logic/util/StatusUpdateTask.java
+++ src/main/java/be/howest/ti/mars/logic/util/StatusUpdateTask.java
@@ -1,11 +1,40 @@
 package be.howest.ti.mars.logic.util;
 
+import be.howest.ti.mars.logic.data.MarsRepository;
+import be.howest.ti.mars.logic.data.Repositories;
+import be.howest.ti.mars.logic.domain.Order;
+import be.howest.ti.mars.logic.domain.Rocket;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.TimerTask;
 
 public class StatusUpdateTask extends TimerTask {
+    private MarsRepository repo = Repositories.H2REPO;
 
     @Override
     public void run() {
+        List<Order> orders = repo.getOrders();
 
+        orders.forEach(order -> {
+            int orderId = order.getOrderId();
+            int currentStatusId = order.getStatusId();
+
+            Rocket rocket = repo.getRocketById(order.getRocketId());
+
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            LocalDateTime now = LocalDateTime.now();
+            LocalDateTime departure = LocalDateTime.parse(rocket.getDeparture(), formatter);
+            LocalDateTime arrival = LocalDateTime.parse(rocket.getArrival(), formatter);
+
+            if (now.isBefore(departure) && currentStatusId != 1) {
+                repo.updateOrderStatus(orderId, 1);
+            } else if (now.isAfter(departure) && now.isBefore(arrival) && currentStatusId != 2) {
+                repo.updateOrderStatus(orderId, 2);
+            } else if (now.isAfter(arrival) && currentStatusId != 3) {
+                repo.updateOrderStatus(orderId, 3);
+            }
+        });
     }
 }
