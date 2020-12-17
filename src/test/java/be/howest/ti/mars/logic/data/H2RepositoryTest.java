179d41a586a319ad0c4289bf871fd5a06f8a9a7c
Thibaut Lenoir <thibaut.lenoir@student.howest.be>
2020-12-17 14:09:05 +0100

    Add test for getting rocket by id

--- src/test/java/be/howest/ti/mars/logic/data/H2RepositoryTest.java
+++ src/test/java/be/howest/ti/mars/logic/data/H2RepositoryTest.java
@@ -173,6 +173,13 @@ public class H2RepositoryTest {
         assertEquals(66, rockets.size());
     }
 
+    @Test
+    void getRocketById() {
+        Rocket rocket = new Rocket(1,"Falcon Heavy", "Mars","2055-12-18 13:30:00", "2055-01-18 08:20:30", 100.0f, 10000.0f, 2700.0f, 10000.0f, 2700.0f);
+
+        assertEquals(rocket, h2Repository.getRocketById(1));
+    }
+
     private void createDatabase() throws IOException {
         executeScript("src/test/resources/testdb-create.sql");
         executeScript("src/test/resources/testdb-populate.sql");
