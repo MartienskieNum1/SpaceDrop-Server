d62c83d5fcd51451c0e369bce4932847d64fee25
Thibaut Lenoir <thibaut.lenoir@student.howest.be>
2020-12-18 11:35:24 +0100

    Add test for getting rockets with normal urgency

--- src/test/java/be/howest/ti/mars/logic/data/H2RepositoryTest.java
+++ src/test/java/be/howest/ti/mars/logic/data/H2RepositoryTest.java
@@ -189,7 +189,7 @@ public class H2RepositoryTest {
     }
 
     @Test
-    void testGetFilteredRocketsFast() {
+    void testGetFilteredRocketsFastUrgency() {
         LocalDateTime[] datesFast = new LocalDateTime[2];
         datesFast[0] = LocalDateTime.of(2055, Month.DECEMBER, 17, 15, 30, 0);
         datesFast[1] = LocalDateTime.of(2055, Month.DECEMBER, 19, 15, 30, 0);
@@ -197,9 +197,26 @@ public class H2RepositoryTest {
             add(new Rocket(1, "Falcon Heavy", "Mars", "2055-12-18 13:30:00", "2055-01-18 08:20:30", 100.0f, 10000.0f, 2700.0f, 10000.0f, 2700.0f));
             add(new Rocket(2, "Shear Razor", "Earth", "2055-12-19 12:15:20", "2055-01-19 22:30:00", 120.0f, 15000.0f, 1100.0f, 15000.0f, 1100.0f));
         }};
-        System.out.println(h2Repository.getFilteredRockets(200, 200, datesFast));
+
         assertEquals(rocketsFast, h2Repository.getFilteredRockets(200, 200, datesFast));
         assertEquals(1, h2Repository.getFilteredRockets(1500, 1500, datesFast).size());
+    }
+
+    @Test
+    void testGetFilteredRocketsNormalUrgency() {
+        LocalDateTime[] datesNormal = new LocalDateTime[2];
+        datesNormal[0] = LocalDateTime.of(2055, Month.DECEMBER, 17, 15, 30, 0);
+        datesNormal[1] = datesNormal[0].plusDays(5);
+        List<Rocket> rocketsNormal = new ArrayList<>() {{
+            add(new Rocket(1, "Falcon Heavy", "Mars", "2055-12-18 13:30:00", "2055-01-18 08:20:30", 100.0f, 10000.0f, 2700.0f, 10000.0f, 2700.0f));
+            add(new Rocket(2, "Shear Razor", "Earth", "2055-12-19 12:15:20", "2055-01-19 22:30:00", 120.0f, 15000.0f, 1100.0f, 15000.0f, 1100.0f));
+            add(new Rocket(3, "Starship", "Mars", "2055-12-20 12:15:20", "2055-01-20 22:30:00", 110.0f, 1600.0f, 1150.0f, 1600.0f, 1150.0f));
+            add(new Rocket(4, "Cataphract", "Earth", "2055-12-21 12:15:20", "2055-01-21 22:30:00", 115.0f, 16000.0f, 5000.0f, 16000.0f, 5000.0f));
+            add(new Rocket(5, "Maiden Voyage", "Mars", "2055-12-22 12:15:20", "2055-01-22 22:30:00", 110.0f, 1300.0f, 560.0f, 1300.0f, 560.0f));
+        }};
+
+        assertEquals(rocketsNormal, h2Repository.getFilteredRockets(200, 200, datesNormal));
+        assertEquals(2, h2Repository.getFilteredRockets(1000, 2000, datesNormal).size());
     }
 
     private void createDatabase() throws IOException {
